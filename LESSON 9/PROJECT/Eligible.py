3c1e26f2cd48e2c9982d88251b7b8e77325d0229
Matthew2-ai <adedoyinakinleye880@gmail.com>
2025-01-04 09:37:36 +0100

    HELLO

---
+++ LESSON 9/PROJECT/Eligible.py
@@ -0,0 +1,11 @@
+if timeinwork<6:00 :- Welldone
+if timeinwork>7:00 :- Excellent
+
+Timeinwork=int(input("Enter time Taken: "))
+
+if timeinwork>=6:
+    print("Welldone")
+
+else:
+    print("Excellent") 
+
