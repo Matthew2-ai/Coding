004ccc0307c9316c2276ea8121d87f1eb4e9b3c2
Matthew2-ai <adedoyinakinleye880@gmail.com>
2025-01-04 17:43:29 +0100

    OOP

---
+++ LESSON 9/PROJECT/OOP.py
@@ -0,0 +1,15 @@
+class worker:
+    grade = 10
+    name = "MATTHEW"
+
+    def introduction(self):
+            print("Hi I am a worker")
+
+
+    def details(self):
+            print("My name is", self.name)
+            print("I work in MathJosh Integrated", self.rank)
+
+ob = worker()
+ob.introduction
+ob.details
