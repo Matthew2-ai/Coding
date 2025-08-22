be723c65b32ad2456dd6912181f5fc011c95b3de
Matthew2-ai <adedoyinakinleye880@gmail.com>
2025-08-22 14:16:06 +0100

    DDD

--- ANNJY/PYTHON/IT2.py
+++ ANNJY/PYTHON/IT2.py
@@ -1,5 +1,5 @@
 def ONsquareTime(n):
-    iteration=0
+    iteration+=0
     for i in range(0,n):
         for j in range(0,n):
             print("*",end=" ")
@@ -7,9 +7,9 @@ def ONsquareTime(n):
         print("")
     print("\nWhen n is ",n"Iterations = ",iteration,"\n")
 
-    OnsquareTime(5)
-    ONsquaretime(4)
-    ONsquareTime(3)
+ONsquareTime(5)
+ONsquaretime(4)
+ONsquareTime(3)
 
-    print("\nWith every 'n' the time taken equals n^2")
-    print("O(n^2)")
+print("\nWith every 'n' the time taken equals n^2")
+print("O(n^2)")
